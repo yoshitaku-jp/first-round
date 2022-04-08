98fae88a9de12dd4b293da78b1352d919c91285e
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2022-04-09 02:39:45 +0900

    root to: 'events#show'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'events#show'
   get 'events/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

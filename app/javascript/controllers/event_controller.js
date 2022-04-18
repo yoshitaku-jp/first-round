0aa9848fcb7c1cfdd7752eb5632b2dd79fb9fde3
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2022-04-18 19:54:08 +0900

    update

--- app/javascript/controllers/event_controller.js
+++ app/javascript/controllers/event_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 import event_channel from "../channels/event_channel";
 
 export default class extends Controller {
-  static targets = ["text_score"];
+  static targets = ["text_score", "user_name", "total_score"];
 
   connect() {
     this.channel = event_channel;
@@ -19,5 +19,11 @@ export default class extends Controller {
     const type = "delete";
 
     this.channel.clear_score(type);
+  }
+
+  sum_score() {
+    console.log("sum score start");
+    this.total_scoreTarget.value = "100";
+    console.log("sum score end");
   }
 }
